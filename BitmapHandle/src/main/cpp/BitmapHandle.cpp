8f23faa1daf32332b9686a378df216f9cc4d88ae
angcyo <angcyo@126.com>
2023-02-28 16:48:55 +0800

    + android log

--- BitmapHandle/src/main/cpp/BitmapHandle.cpp
+++ BitmapHandle/src/main/cpp/BitmapHandle.cpp
@@ -1,10 +1,24 @@
 #include <jni.h>
 #include <string>
+#include "android-log.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_angcyo_bitmap_handle_BitmapHandle_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++ 123";
+
+    LOGW("Test...");
+
     return env->NewStringUTF(hello.c_str());
 }
+
+//region ---init---
+
+extern "C" JNIEXPORT JNICALL
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    LOGD("So初始化:%s %s", vm, reserved);
+    return JNI_VERSION_1_6;
+}
+
+//endregion ---init---
---
+++ BitmapHandle/src/main/cpp/android-log.h
@@ -0,0 +1,27 @@
+//
+// Created by angcyo on 2023/2/28.
+//
+
+#ifndef JNI_DEBUG_H
+#define JNI_DEBUG_H
+
+#include <android/log.h>
+
+#define MODULE_NAME "bitmap-handle-jni"
+#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, MODULE_NAME, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, MODULE_NAME, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, MODULE_NAME, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, MODULE_NAME, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, MODULE_NAME, __VA_ARGS__)
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, MODULE_NAME, __VA_ARGS__)
+
+#else
+
+#define LOGV(...)
+#define LOGD(...)
+#define LOGI(...)
+#define LOGW(...)
+#define LOGE(...)
+#define LOGF(...)
+
+#endif //JNI_DEBUG_H
